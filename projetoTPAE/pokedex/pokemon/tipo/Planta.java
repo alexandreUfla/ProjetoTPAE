9ebd4e6fac4a12d697bbfc16b5e82b2362fd8c24
alexandreUfla <153199168+alexandreUfla@users.noreply.github.com>
2024-08-08 11:48:00 -0300

    Update and rename planta.java to Planta.java

---
+++ projetoTPAE/pokedex/pokemon/tipo/Planta.java
@@ -0,0 +1,71 @@
+package pokedex.pokemon.tipo;
+
+/**
+ * A classe {@code Planta} representa um Pokémon do tipo Planta e contém métodos para calcular
+ * o dano causado e recebido em batalhas com outros tipos de Pokémon.
+ * <p>
+ * Esta classe contém três métodos principais:
+ * <ul>
+ * <li>{@link #getAtaqueNormal(String)} - calcula o dano de um ataque normal.
+ * <li>{@link #getAtaquePrincipal(String)} - calcula o dano de um ataque principal.
+ * <li>{@link #getDanoRecebido(String)} - calcula o dano recebido de um ataque de outro Pokémon.
+ * </ul>
+ */
+public class Planta {
+    
+    private int ataqueNormal;
+    private int ataquePrincipal;
+    private int danoRecebido;
+
+    /**
+     * Calcula o valor do ataque normal baseado no tipo do oponente.
+     *
+     * @param oponente o tipo do Pokémon oponente (ex: "planta", "fogo").
+     * @return o valor do ataque normal.
+     */
+    public int getAtaqueNormal(String oponente) {
+        if(oponente.equals("planta")) {
+            this.ataqueNormal = 2;
+        } else if(oponente.equals("fogo")) {
+            this.ataqueNormal = 2;
+        } else {
+            this.ataqueNormal = 3;
+        }
+        return this.ataqueNormal;
+    }
+
+    /**
+     * Calcula o valor do ataque principal baseado no tipo do oponente.
+     *
+     * @param oponente o tipo do Pokémon oponente (ex: "planta", "fogo").
+     * @return o valor do ataque principal.
+     */
+    public int getAtaquePrincipal(String oponente) {
+        if(oponente.equals("planta")) {
+            this.ataquePrincipal = 4;
+        } else if(oponente.equals("fogo")) {
+            this.ataquePrincipal = 3;
+        } else {
+            this.ataquePrincipal = 5;
+        }
+        return this.ataquePrincipal;
+    }
+
+    /**
+     * Calcula o valor do dano recebido baseado no tipo do oponente.
+     *
+     * @param oponente o tipo do Pokémon oponente (ex: "planta", "fogo").
+     * @return o valor do dano recebido.
+     */
+    public int getDanoRecebido(String oponente) {
+        if(oponente.equals("planta")) {
+            this.danoRecebido = 4;
+        } else if(oponente.equals("fogo")) {
+            this.danoRecebido = 5;
+        } else {
+            this.danoRecebido = 3;
+        }
+        return danoRecebido;
+    }
+    
+}
